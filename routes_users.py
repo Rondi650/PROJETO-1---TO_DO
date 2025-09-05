080c6b6413dcda9be6aaeff29e5fdc7089f95eb2
Rondi650 <rondinellenunes123@gmail.com>
2025-09-05 20:57:23 -0300

    identado rota usuarios

--- routes_users.py
+++ routes_users.py
@@ -19,19 +19,21 @@ def cadastrar():
 @app.route('/cadastrar_usuario', methods=['POST'])
 def cadastrar_usuario():
     form = FormularioUsuario(request.form)
-    
+
     if form.validate_on_submit():
-    
         nome = form.nome.data
         email = form.email.data
         senha = form.senha.data
-        
+
         novo_usuario = Usuarios(nome=nome, email=email)
         novo_usuario.set_senha(senha)
+
         db.session.add(novo_usuario)
         db.session.commit()
+
         flash(f'{novo_usuario.nome} cadastrado com sucesso!', 'success')
         return redirect(url_for('login'))
+
     return render_template('cadastrar.html', titulo='Cadastro', form=form)
 
 @app.route('/autenticar', methods=['POST'])
@@ -40,9 +42,10 @@ def autenticar():
     email = form.email.data
     usuario = Usuarios.query.filter_by(email=email).first()
     senha = check_password_hash(usuario.senha, form.senha.data)
+    
     if usuario and senha:
         session['usuario_id'] = usuario.id
-        flash(usuario.nome + ' logado com sucesso!', 'success')
+        flash(f'{usuario.nome} logado com sucesso!', 'success')
         return redirect(url_for('index'))
     else:
         flash('E-mail ou senha incorretos.', 'danger')
